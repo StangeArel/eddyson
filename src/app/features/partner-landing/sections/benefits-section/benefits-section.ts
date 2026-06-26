174f71df30fcc9c420b522454885647f7342226d
Valeriya <valeriya.stange@gmail.com>
2026-06-27 00:28:13 +0300

    Add benefits section HTML content

--- src/app/features/partner-landing/sections/benefits-section/benefits-section.ts
+++ src/app/features/partner-landing/sections/benefits-section/benefits-section.ts
@@ -1,9 +1,8 @@
 import { Component } from '@angular/core';
-import { BenefitCard } from '../../ui/benefit-card/benefit-card';
 
 @Component({
   selector: 'app-benefits-section',
-  imports: [BenefitCard],
+  imports: [],
   templateUrl: './benefits-section.html',
   styleUrl: './benefits-section.scss',
 })
